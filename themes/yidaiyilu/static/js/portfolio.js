0e31b4dbcff4283c2771df182ee0236780502b8f
hugo <hugo@iZ23ej0v5wbZ.(none)>
2015-12-01 13:13:53 +0800

    save

---
+++ themes/yidaiyilu/static/js/portfolio.js
@@ -0,0 +1,17 @@
+/*
+    This script was originally part of David Millers (https://github.com/davidtmiller)
+    Bootstrap theme "Freelancer" add some functionality to the contact form.
+
+    Licensed under the Apache License 2.0. For more information take a look at the orignal
+    repository unde https://github.com/IronSummitMedia/startbootstrap-freelancer.
+*/
+
+$(function() {
+    $(window).load(function() {
+      $('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails"
+      });
+    });
+});
+
